4c700c92a376957daba0377b99b7c4e866856b01
ingrafa <38500731+ingrafa@users.noreply.github.com>
2021-01-12 14:27:21 -0500

    Commmi realizadompor Rafael Angamarca

--- src/ec/ups/edu/Modelo/Logica.java
+++ src/ec/ups/edu/Modelo/Logica.java
@@ -7,8 +7,9 @@ public class Logica {
 	public String metodoIntegrante2() {
 		return "integrante2";
 	}
+
 		public String metodoIntegrante3() {
-			return "integrante3";
+			return "Este metodo fue cambiado por Rafael Angamarca";
 
 }
 }
