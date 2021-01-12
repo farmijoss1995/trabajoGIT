bd190d699024b0bfc65f115d1438ba518beaa36c
farmijoss1995 <62902977+farmijoss1995@users.noreply.github.com>
2021-01-12 11:53:49 -0500

    Proyecto Base

---
+++ src/ec/ups/edu/Modelo/Logica.java
@@ -0,0 +1,14 @@
+package ec.ups.edu.Modelo;
+
+public class Logica {
+	public String metodoIntegrante1() {
+		return "integrante1";
+	}
+	public String metodoIntegrante2() {
+		return "integrante2";
+	}
+		public String metodoIntegrante3() {
+			return "integrante3";
+
+}
+}
---
+++ src/ec/ups/edu/Modelo/Principal.java
@@ -0,0 +1,10 @@
+package ec.ups.edu.Modelo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Logica l=new Logica();
+	}
+
+}
